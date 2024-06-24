8c49404de55f94b218f9854fa92798a8075c95c8
weijchen <weijchen@visionnav.com>
2024-06-24 15:01:08 +0800

    [bugfix] 修复sim_data_flow没有编译的错误

--- app/P/controller_P/master/P_master.cpp
+++ app/P/controller_P/master/P_master.cpp
@@ -93,11 +93,11 @@ void AGVController::whileSpin() {
 }
 
 void AGVController::pubTransferSpin() {
-    std::string msg_str = transfer_ptr_->getTransfer().dump();
-    std::vector<uint8_t> buf(msg_str.begin(), msg_str.end());
+    // std::string msg_str = transfer_ptr_->getTransfer().dump();
+    // std::vector<uint8_t> buf(msg_str.begin(), msg_str.end());
 
-    //publish
-    ecal_ptr_->send("webot/transfer", buf.data(), buf.size());
+    // //publish
+    // ecal_ptr_->send("webot/transfer", buf.data(), buf.size());
 }
 
 void AGVController::pubRobotPoseSpin() {
@@ -136,7 +136,7 @@ void AGVController::pubSerialSpin() {
     sim_data_flow::PMsgUp payload;
 
     payload.set_timestamp(time_stamp_);
-    payload.set_forkposez(fork_ptr_->getSenosorValue());
+    payload.set_forkposeZ(fork_ptr_->getSenosorValue());
     payload.set_steerposition(stree_ptr_->getSenosorValue());
 
     payload.set_l_wheel(l_ptr_->getWheelArcLength());
--- base/sim_data_flow/build.py
+++ base/sim_data_flow/build.py
@@ -3,7 +3,7 @@ import os
 os.chdir("../")
 os.system("ls")
 # cmd="protoc --proto_path=./ --proto_path=./sim_data_flow --proto_path=./foxglove-vn/foxglove-vn/  --cpp_out=./sim_data_flow/build ./sim_data_flow/*.proto"
-cmd="protoc --proto_path=./sim_data_flow --proto_path=./foxglove-vn/foxglove-vn --cpp_out=./sim_data_flow/build ./sim_data_flow/*.proto"
+cmd="protoc --proto_path=./sim_data_flow/sim_data_flow --proto_path=./foxglove-vn/foxglove-vn --cpp_out=./sim_data_flow/build ./sim_data_flow/sim_data_flow/*.proto"
 os.system(cmd)
 
 
