3af985028ee0983478b73921d377bfedea627091
cnick <530102839@qq.com>
2024-10-21 11:37:44 +0800

    [bugfix] 修复编译脚本错误

--- app/common/shadow/shadow_show_gui/main.cpp
+++ app/common/shadow/shadow_show_gui/main.cpp
@@ -8,19 +8,17 @@ using namespace VNSim;
 int main(int argc, char *argv[]) {
     // init glog
     // TODO:FIXME path....
-    g_Logger.initLog("../../configs/log_config/webots_log_config.ini","/home/visionnav/logs/gui.log");
+    g_Logger.initLog("../../configs/log_config/webots_log_config.ini", "/home/visionnav/logs/gui.log");
     LOG_INFO("log init...");
     QApplication a(argc, argv);
     // init ctrl
-    std::shared_ptr<BaseController> ctrl_ptr =
-        std::make_shared<AGVController>();
+    std::shared_ptr<BaseController> ctrl_ptr = std::make_shared<AGVController>();
 
     ctrl_ptr->start();
 
     QObject::connect(ctrl_ptr.get(), SIGNAL(finished()), &a, SLOT(quit()));
 
     a.exec();
-    // LOG_INFO("try stop svc_P");
-    // system("killall svc_P");
+    
     return 0;
 }
